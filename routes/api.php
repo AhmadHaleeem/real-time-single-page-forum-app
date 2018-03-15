d5e37e126b9856be4d53742a8cc59f1fb4e87591
AhmadHaleem <coldman9910@gmail.com>
2018-03-15 13:07:11 +0100

    like dislike api

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
 
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unlikeIt');
+
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
